cf70deb22d37c2592139603a7837c7367376166c
mariotheguy <mariodandrea14@yahoo.com>
2021-04-25 17:06:29 -0400

    Added mad labs catalog

---
+++ src/importer/madlabs.js
@@ -0,0 +1,14 @@
+const { scrapFrom } = require('../scraper/gdoc');
+const { launcher } = require('../utils');
+
+const scrap = scrapFrom('176oXSO83YXhYkVDBpoanyiroWaqXq7SmOY8OQfO6bfY', {
+  name: 'Mad Labs Keycaps',
+  instagram: 'https://www.instagram.com/mad_labs_keycaps/',
+  website: 'https://www.madlabskeycaps.com/online-store',
+});
+
+launcher(scrap);
+
+module.exports = {
+  scrap,
+};
