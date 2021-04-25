789f97bf26ca2a69e19fffe07d3f99515861feeb
mariotheguy <mariodandrea14@yahoo.com>
2021-04-25 17:11:22 -0400

    changed website url to homepage

--- src/importer/madlabs.js
+++ src/importer/madlabs.js
@@ -4,7 +4,7 @@ const { launcher } = require('../utils');
 const scrap = scrapFrom('176oXSO83YXhYkVDBpoanyiroWaqXq7SmOY8OQfO6bfY', {
   name: 'Mad Labs Keycaps',
   instagram: 'https://www.instagram.com/mad_labs_keycaps/',
-  website: 'https://www.madlabskeycaps.com/online-store',
+  website: 'https://www.madlabskeycaps.com/',
 });
 
 launcher(scrap);
